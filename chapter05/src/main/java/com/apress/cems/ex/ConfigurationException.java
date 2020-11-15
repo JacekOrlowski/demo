69a27012d9af623bede6ee79d21ab7713d8f0adc
jorlowski1 <jorlowski1@sii.pl>
2020-11-15 21:08:28 +0100

    Adding maven-compiler-plugin configuration.

--- chapter05/src/main/java/com/apress/cems/ex/ConfigurationException.java
+++ chapter05/src/main/java/com/apress/cems/ex/ConfigurationException.java
@@ -36,7 +36,6 @@ public class ConfigurationException extends RuntimeException {
     public ConfigurationException(String message) {
         super(message);
     }
-
     public ConfigurationException(String message, Throwable cause) {
         super(message, cause);
     }
